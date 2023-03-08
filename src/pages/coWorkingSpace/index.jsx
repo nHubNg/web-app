498833ba1514b16e8b1e04ced4f28074cbf98848
Desmond-Fon <desmondfon@yahoo.com>
2023-03-08 05:20:05 +0100

    finished co-working page

--- src/pages/coWorkingSpace/index.jsx
+++ src/pages/coWorkingSpace/index.jsx
@@ -3,11 +3,17 @@ import Table from 'react-bootstrap/Table';
 import Footer from '../../components/footer';
 import FeatureSection from '../../components/featureSection';
 import bg from '../../img/hub/space.JPG';
+import room1 from '../../img/hub/room1.JPG';
+import room2 from '../../img/hub/room2.JPG';
+import room3 from '../../img/hub/room3.JPG';
+import room4 from '../../img/hub/room4.JPG';
+import room5 from '../../img/hub/room5.JPG';
+import room6 from '../../img/hub/room6.JPG';
 
 const CoWorkingSpace = () => {
   return (
     <>
-      <Header name="Co-Working Space" bg={bg}/>
+      <Header name="Co-Working Space" bg={bg} />
       <div className="container">
         <div className="title-wrap mb-5" data-aos="fade-up">
           <h2 className="section-title co-working-title">
@@ -88,6 +94,61 @@ const CoWorkingSpace = () => {
         <div className="co-working-div">
           <h5>Virtual Office - &#8358;100,000/Year</h5>
           <h5>Shared Office Space - &#8358;20,000/Month</h5>
+        </div>
+
+        <div className="co-working-div">
+          <h5>
+            We offer free internet services, constant power supply and backup
+            power supply to our clients so as to ensure a seamless and
+            satisfactory experience.
+          </h5>
+        </div>
+
+        <div className="about-staff">
+          <h3>TAKE A LOOK AT OUR CO WORKING SPACE</h3>
+        </div>
+
+        <div className="row text-center mb-4">
+          <div className="col-md-4 col-sm-6 mt-4" data-aos="fade-up">
+            <img src={room1} alt="" />
+          </div>
+          {/* <!-- /.col-md-4 col-sm-6  --> */}
+          <div
+            className="col-md-4 col-sm-6 mt-4"
+            data-aos="fade-up"
+            data-aos-delay="200"
+          >
+            <img src={room2} alt="" />
+          </div>
+          {/* <!-- /.col-md-4 col-sm-6  --> */}
+          <div
+            className="col-md-4 col-sm-6 mt-4"
+            data-aos="fade-up"
+            data-aos-delay="400"
+          >
+            <img src={room3} alt="" />
+          </div>
+          <div
+            className="col-md-4 col-sm-6 mt-4"
+            data-aos="fade-up"
+            data-aos-delay="400"
+          >
+            <img src={room4} alt="" />
+          </div>
+          <div
+            className="col-md-4 col-sm-6 mt-4"
+            data-aos="fade-up"
+            data-aos-delay="400"
+          >
+            <img src={room5} alt="" />
+          </div>
+          <div
+            className="col-md-4 col-sm-6 mt-4"
+            data-aos="fade-up"
+            data-aos-delay="400"
+          >
+            <img src={room6} alt="" />
+          </div>
         </div>
       </div>
 
